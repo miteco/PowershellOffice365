45548fcd8f17c5665b082e12dc05d83707515b14
lfvicente <lfvicente@gmail.com>
2019-05-22 12:28:24 +0200

    Entrega 190522

--- main.ps1
+++ main.ps1
@@ -1,80 +1,21 @@
-Function NewCSV {
-    Write-Host "Ha seleccionado la carga masiva desde CSV"
-    Write-Host ""
-    Read-Host -Prompt 'Pulse Intro para continuar'
+Clear-Host
+
+# Abrimos la conexi?n con azureAD en caso de no estar conectado
+if($azureConnection.Account -eq $null){
+    $azureConnection = Connect-AzureAD
+}    
+
+# Ficheros requeridos con Funciones auxiliares 
+$ScriptDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+try {
+    . ("$ScriptDirectory\menu.ps1")    
+    . ("$ScriptDirectory\commonFunctions.ps1")  
+    . ("$ScriptDirectory\RandomPasswords.ps1")         
 }
-
-Function UpdateCSV {
-    Write-Host "Ha seleccionado actualización masiva desde CSV"
-    Write-Host ""
-    Read-Host -Prompt 'Pulse Intro para continuar'
+catch {
+    Write-Host "Error cargando ficheros auxiliares PowerShell Scripts" -ForegroundColor Red
 }
+#endregion
 
-Function SearchUserByDNI{
-    Write-Host "Ha seleccionado actualización masiva desde CSV"
-    Write-Host ""
-    Read-Host -Prompt 'Pulse Intro para continuar'
-}
-
-Function SearchUserByDistinguisedName{
-    Write-Host "Ha seleccionado actualización masiva desde CSV"
-    Write-Host ""
-    Read-Host -Prompt 'Pulse Intro para continuar'
-}
-Function UpdateUserByDNI{
-    Write-Host "Ha seleccionado actualización masiva desde CSV"
-    Write-Host ""
-    Read-Host -Prompt 'Pulse Intro para continuar'
-}
-
-Function UpdateUserByDistinguisedName{
-    Write-Host "Ha seleccionado actualización masiva desde CSV"
-    Write-Host ""
-    Read-Host -Prompt 'Pulse Intro para continuar'
-}
-
-
-Function main {
-    Clear-Host
-
-    Do {
-        Write-Host "Menu de identidades"
-        Write-Host "==================="
-        Write-Host ""
-        Write-Host "1. Buscar usuario por DNI"
-        Write-Host "2. Buscar usuario por distinguisedName"
-        Write-Host "3. Editar usuario por DNI"
-        Write-Host "4. Editar usuario por distinguisedName"
-        Write-Host "5. Altas desde CSV"
-        Write-Host "6. Editar desde CSV"
-        Write-Host ""
-        Write-Host "E. Salir"
-        Write-Host ""
-        $opt=Read-Host -Prompt 'Introduzca opcion'
-
-        Clear-Host
-        if($opt -eq 1){          
-            SearchUserByDNI
-        } elseif ($opt -eq 2) {
-            SearchUserByDistinguisedName
-        } elseif ($opt -eq 3) {
-            UpdateUserByDNI
-        } elseif ($opt -eq 4) {
-            UpdateUserByDistinguisedName
-        } elseif ($opt -eq 5) {
-            NewCSV
-        } elseif ($opt -eq 6) {
-            UpdateCSV
-        } elseif ($opt -eq "E") {
-            Write-Host "Adios!!"                
-        }
-
-    } While($opt -ne "E") 
-
-    
-    
-    Exit-PSHostProcess
-
-}
-
-main
+# lanzamos la aplicaci?n llamando al men? principal
+showMainMenu
---
+++ menu.ps1
@@ -0,0 +1,113 @@
+﻿<#
+    Autor:  Luis Felipe Vicente Díez
+    Fecha:  22 de Mayo de 2019
+    Desc:   Generación de un menú para la GUI de aplicación
+#>
+
+Function Invoke-Menu {
+    [cmdletbinding()]
+    Param(
+    [Parameter(Position=0,Mandatory=$True,HelpMessage="Enter your menu text")]
+    [ValidateNotNullOrEmpty()]
+    [string]$Menu,
+    [Parameter(Position=1)]
+    [ValidateNotNullOrEmpty()]
+    [string]$Title = "Menu",
+    [Alias("cls")]
+    [switch]$ClearScreen
+    )
+     
+    #clear the screen if requested
+    if ($ClearScreen) { 
+     Clear-Host 
+    }
+     
+    #build the menu prompt
+    $menuPrompt = $title
+    #add a return
+    $menuprompt+="`n"
+    #add an underline
+    $menuprompt+="-"*$title.Length
+    #add another return
+    $menuprompt+="`n"
+    #add the menu
+    $menuPrompt+=$menu
+     
+    Read-Host -Prompt $menuprompt
+     
+    } #end function
+
+$menu=@"
+
+1. Buscar usuario por DNI
+2. Buscar usuario por distinguisedName
+3. Editar DNI de usuario 
+4. Editar usuario por distinguisedName
+5. Altas desde CSV
+6. Editar desde CSV
+7. Exportar usuarios a CSV
+
+R. Conectar con AzureAD
+Q. Salir
+
+Seleccione el numero de Tarea o Q para Salir
+"@
+
+function showMainMenu {
+    
+    Do {
+        Clear-Host
+        #use a Switch construct to take action depending on what menu choice
+        #is selected.
+        Switch (Invoke-Menu -menu $menu -title "Tareas de Azure AD" -clear ) {
+        "1" {   Start-Sleep -seconds 1                 
+                Clear-Host
+                Write-Host "Buscar Usuario por DNI" -ForegroundColor Yellow
+                FindUserByDNI
+            } 
+        "2" {   Start-Sleep -seconds 1
+                Clear-Host
+                Write-Host "Buscar Usuario por DistinguisedName" -ForegroundColor Green                
+                FindUserByDistinguisedName
+            }
+        "3" {   Start-Sleep -seconds 1
+                Clear-Host
+                Write-Host "Actualizar DNI de un Usuario por DistinguisedName" -ForegroundColor  Magenta
+                UpdateDniByUser
+            }
+        "4" {   Start-Sleep -seconds 1
+                Clear-Host
+                Write-Host "Actualizar Usuario DistinguisedName OPCIÓN NO DISPONIBLE" -ForegroundColor  Red
+                Start-Sleep -seconds 1
+            }
+        "5" {   Start-Sleep -seconds 1
+                Clear-Host
+                Write-Host "Altas desde CSV" -ForegroundColor  White
+                NewUsersCSV
+            }
+        "6" {   Start-Sleep -seconds 1
+                Clear-Host
+                Write-Host "Modificaciones desde CSV" -ForegroundColor  Blue
+                EditUsersCSV
+            }
+        "7" {   Start-Sleep -seconds 1
+                Clear-Host
+                Write-Host "Exportar Usuarios a CSV" -ForegroundColor  Cyan                
+                exportUsersCSV
+            }
+        "R" {   Clear-Host
+                Write-Host "Conectando con AzureAD" -ForegroundColor Cyan
+                Start-Sleep -seconds 1
+                Connect-AzureAD
+            }
+        "Q" {   Clear-Host
+                Write-Host "¡Adios!" -ForegroundColor Cyan     
+                Return       
+                #Exit-PSHostProcess
+            }
+        Default {   Write-Warning "Opción invalida. Intentelo de nuevo."
+                    Start-Sleep -milliseconds 750}
+        } #switch
+    } While ($True)
+
+}
